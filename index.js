271e30a029636b998e9fa924dfbf6fe7f4d027d8
Oxsav <vmfamaral@gmail.com>
2016-03-17 21:49:22 +0000

    added last enpoints for favorites

--- index.js
+++ index.js
@@ -123,7 +123,7 @@ router.get('/musics/:id', function(req, res) {
     else{
 		res.statusCode = 204;
     }
-    res.send(music)
+    res.send(music);
     
 });
 
@@ -187,28 +187,21 @@ router.get('/users/:userid/musics', function(req, res, next) {
     	res.statusCode = 204;
     	res.send({body: usermusics})
     }else{
-    	
+        var usermusics = [];
+
+        for(id in musicids){
+            var music = _.find(musics, function(o){
+                return o.id == musicids[id].musicid;
+            }); 
+            
+            usermusics.push(music);
+        }
+        
+        res.statusCode = 200;
+        res.send(usermusics);  
+        
     }
 
-    var usermusics = [];
-
-    for(id in musicids){
-    	console.log("get user musics ", musicids[id]);
-    	var music = _.find(musics, function(o){
-    		return o.id == musicids[id].musicid;
-    	});	
-    	
-    	usermusics.push(music);
-    }
-    
-
-    
-    else{
-    	res.statusCode = 204;
-    	res.send({body: usermusics})	
-    }
-
-    next();
 });
 
 
@@ -216,9 +209,44 @@ router.get('/users/:userid/musics', function(req, res, next) {
  *  CREATE FAVORITE
  */
 
-router.post('/users/:userid/musics/:musicid', function(req, res, next) {
-    
-    
+router.post('/users/:userid/musics/', function(req, res, next) {
+    var requserid = req.params.userid;
+    var reqmusicid = req.body.musicid;
+
+    var usermatch = _.find(users, function(o){
+        return o.id == requserid;
+    });
+
+    var musicmatch = _.find(musics, function(o){
+        return o.id == reqmusicid;
+    });
+
+    var match = _.find(favorites, function(o){
+        return (o.userid == req.params.userid && o.musicid == req.params.musicid)
+    });
+
+    if(!usermatch){
+        res.statusCode = 400;
+        res.send({"message":"User does not exist"});
+    }
+    else if(!musicmatch){
+        res.statusCode = 400;
+        res.send({"message":"Music does not exist"});   
+    }
+    else if(!match){
+        var newfavorite = {
+            userid: req.params.userid,
+            musicid: req.body.musicid
+        }
+
+        favorites.push(newfavorite);
+        res.statusCode = 200;
+        res.send({"message":"Added favorite"});
+    }
+    else{
+        res.statusCode = 409;
+        res.send({"message":"User already have that favorite"});
+    }
 });
 
 
@@ -227,11 +255,23 @@ router.post('/users/:userid/musics/:musicid', function(req, res, next) {
  */
 
 router.delete('/users/:userid/musics/:musicid', function(req, res) {
- 	
+ 	var requserid = req.params.userid;
+    var reqmusicid = req.params.musicid;
+
+    var index = _.findIndex(favorites, function(o){
+        return (o.userid == requserid && o.musicid == reqmusicid);
+    })
+
+    if(index != -1){
+        _.pullAt(favorites, index);
+        res.statusCode = 200;
+        res.send({"message":"Deleted com success!"});
+    }
+    else{
+        res.statusCode = 404;
+        res.send({"message":"Match not found!"});   
+    }
 });
-
-
-
 
 // more routes for our API will happen here
 
@@ -242,3 +282,5 @@ app.use('/api', router);
 // START THE SERVER
 // =============================================================================
 app.listen(port);
+
+console.log("Server Listening : http://localhost:" + port + "/api");
